38cd829297361db1ee2cf4b0302f4af43389427d
Nathalia Roberts <31518187+NJRoberts@users.noreply.github.com>
2019-11-18 11:39:26 -0800

    trying different path to CartoDB proxy

--- php/callProxy.php
+++ php/callProxy.php
@@ -1,5 +1,5 @@
 <?php
-include 'https://github.com/NJRoberts/tarantula-report/blob/master/php/cartoDBProxy.php';
+include 'php/cartoDBProxy.php';
 //			^CHANGE THIS TO THE PATH TO YOUR cartodbProxy.php
 $queryURL = $_POST['qurl'];
 $return = goProxy($queryURL);
